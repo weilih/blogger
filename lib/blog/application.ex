676b0f734a5af1adc4fc1d07370682c2213468f1
willieLOH <willie.loh@live.com>
2017-04-08 14:58:08 +0800

    mix new
    mix new blogger —sup —app blog
    mix ecto.gen.repo -r Blogger.Repo

---
+++ lib/blog/application.ex
@@ -0,0 +1,23 @@
+defmodule Blog.Application do
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: Blog.Worker.start_link(arg1, arg2, arg3)
+      # worker(Blog.Worker, [arg1, arg2, arg3]),
+      supervisor(Blog.Repo, [])
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Blog.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
